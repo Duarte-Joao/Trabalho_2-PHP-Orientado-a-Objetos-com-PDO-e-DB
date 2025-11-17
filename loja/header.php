0605fe84c630d0d71cf79cdb023e2c7968d6f28c
João Eduardo Dias Duarte <joaoeddgamer@gmail.com>
2025-11-17 09:51:18 -0300

    adicionando arquivos php e organizando estrutura de pastas

---
+++ loja/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Loja Chapecoense</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Admin Loja</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="site/admin/produto/ProdutoList.php">Produtos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="site/admin/cliente/ClienteList.php">Clientes</a></li>
+                    <li class="nav-item"><a class="nav-link" href="site/admin/funcionario/FuncionarioList.php">Funcionarios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="site/admin/usuario/UsuarioList.php">Usuarios</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ loja/index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+<div class="container mt-4">
+    <h2>Bem-vindo ao Sistema da Loja Chapecoense</h2>
+    <p>Escolha uma opção no menu acima.</p>
+</div>
+
+<?php include 'footer.php'; ?>
